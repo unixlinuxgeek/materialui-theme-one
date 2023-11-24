93339722b2ba75a05c013e8ac080f81382f371d4
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-11-24 15:50:18 +0500

    revision: Main menu

--- src/app/comp/MiniDrawer.tsx
+++ src/app/comp/MiniDrawer.tsx
@@ -20,10 +20,13 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
+import HomeIcon from '@mui/icons-material/Home';
+import LangIcon from '@mui/icons-material/Language';
+import PhIcn from '@mui/icons-material/Phone';
+import GrdIcn from '@mui/icons-material/GridView';
 
-const drawerWidth = 240;
+const drawerWidth = 200;
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
@@ -100,9 +103,19 @@ export default function MiniDrawer() {
   
   const darkTheme = createTheme({
     palette: {
-      primary: {
-        main: red[500],
-      },
+        mode: 'dark',
+        // primary: {
+        //     light: '#757ce8',
+        //     main: '#3f50b5',
+        //     dark: '#002884',
+        //     contrastText: '#fff',
+        //   },
+        //   secondary: {
+        //     light: '#ff7961',
+        //     main: '#f44336',
+        //     dark: '#ba000d',
+        //     contrastText: '#000',
+        //   },
     },
   });
 
@@ -128,6 +141,7 @@ export default function MiniDrawer() {
 //theme = responsiveFontSizes(theme);
 
   return (
+    <ThemeProvider theme={darkTheme}>
     <Box sx={{ display: 'flex', ml: 8  }}>
       <CssBaseline />
       {/* <AppBar position="fixed" open={open}>
@@ -157,7 +171,7 @@ export default function MiniDrawer() {
         </DrawerHeader>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Главная', 'Заметки', 'Проекты','Контакты' ].map((text, i) => (
             <ListItem key={text} disablePadding sx={{ display: 'block' }}>
               <ListItemButton
                 sx={{
@@ -173,38 +187,19 @@ export default function MiniDrawer() {
                     justifyContent: 'center',
                   }}
                 >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {i == 0 ? <HomeIcon /> : <></>}
+                  {i == 1 ? <LangIcon /> : <></>}
+                  {i == 2 ? <GrdIcn/>: <></>}
+                  {i == 3 ? <PhIcn/>: <></>}
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
+              <Divider />
             </ListItem>
+            
           ))}
         </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+    
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
@@ -237,5 +232,6 @@ export default function MiniDrawer() {
         </Typography>
       </Box>
     </Box>
+    </ThemeProvider>
   );
 }
